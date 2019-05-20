1185a88bac5747fa91430d4d7442d3e0c876f9dd
Kevin J Crespin <kevinjonathan290@gmail.com>
2019-05-20 13:00:10 -0700

    Update userIncreaseBalance.php

--- src/userIncreaseBalance.php
+++ src/userIncreaseBalance.php
@@ -1,14 +1,14 @@
 <?php
 	include("connectDatabase.php");
-
-    $userBalanceQuery = "SELECT balance FROM users WHERE email='" . $_GET["email"] . "';";
-    $userBalanceObject = mysqli_query($connect, $userBalanceQuery);
-    $userBalanceArray = mysqli_fetch_assoc($userBalanceObject);
+	
+	$userBalanceQuery = "SELECT balance FROM users WHERE email='" . $_GET["email"] . "';";
+   	$userBalanceObject = mysqli_query($connect, $userBalanceQuery);
+    	$userBalanceArray = mysqli_fetch_assoc($userBalanceObject);
     
-    $b = $userBalanceArray["balance"] + 100;
+    	$b = $userBalanceArray["balance"] + 100;
 
 	$uIBQuery = "UPDATE users SET balance = '" . $b . "' WHERE email='" . $_GET["email"] . "'";
-    $uIBObject = mysqli_query($connect, $uIBQuery);
+    	$uIBObject = mysqli_query($connect, $uIBQuery);
     
 	header("Location: /BookExchange/admin.php");
 ?>
